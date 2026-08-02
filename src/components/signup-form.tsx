fc98bb905a3d19cbb0145aba4e3dc16f21a474c6
Andrey Polulyakh <sQrun1217@mail.ru>
2026-08-02 16:27:18 +1000

    feat(auth): implement user signup

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,11 +1,8 @@
+"use client";
+
+import { SignupState, signupAction } from "@/app/signup/actions";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
@@ -13,35 +10,51 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useActionState } from "react";
 
 export const SignupForm = ({ ...props }: React.ComponentProps<typeof Card>) => {
+  const [state, formAction] = useActionState<SignupState | null, FormData>(
+    signupAction,
+    null
+  );
+
   return (
     <Card {...props}>
       <CardHeader>
         <CardTitle>Create an account</CardTitle>
       </CardHeader>
       <CardContent>
-        <form>
+        <form action={formAction}>
           <FieldGroup>
             <Field>
               <FieldLabel htmlFor="name">Full Name</FieldLabel>
-              <Input id="name" type="text" placeholder="John Doe" required />
+              <Input
+                id="name"
+                name="name"
+                type="text"
+                placeholder="John Doe"
+                required
+              />
             </Field>
             <Field>
               <FieldLabel htmlFor="email">Email</FieldLabel>
               <Input
                 id="email"
                 type="email"
+                name="email"
                 placeholder="example@example.com"
                 required
               />
             </Field>
             <Field>
               <FieldLabel htmlFor="password">Password</FieldLabel>
-              <Input id="password" type="password" required />
+              <Input id="password" name="password" type="password" required />
               <FieldDescription>
                 Must be at least 8 characters long.
               </FieldDescription>
+              {state?.error && (
+                <p>{state?.error}</p>
+              )}
             </Field>
             <FieldGroup>
               <Field>
